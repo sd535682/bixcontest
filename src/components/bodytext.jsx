049742e8a8b0abeb75edb69db7a688cb9a9746f3
sd535682 <sd535682@gmail.com>
2024-09-16 13:35:13 +0530

    bug fix

--- src/components/bodytext.jsx
+++ src/components/bodytext.jsx
@@ -1,7 +1,3 @@
 export default function BodyText({ text, color }) {
-  return (
-    <>
-      <h1 className={`${color} text-lg font-semibold `}>{text}</h1>
-    </>
-  );
+  return <h1 className={`${color} text-lg font-semibold `}>{text}</h1>;
 }
